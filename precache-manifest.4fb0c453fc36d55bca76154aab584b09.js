fb106399598d1c556e4dbf4aca192c9bd37ae0b6
HIEU QUANG <hieuquang2212@gmail.com>
2019-12-13 16:58:29 +0700

    Updates

---
+++ precache-manifest.4fb0c453fc36d55bca76154aab584b09.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5efa48c047ce90ae4a6e0a3e75a12056",
+    "url": "/deploy-react/index.html"
+  },
+  {
+    "revision": "7994ebc31a9695bec455",
+    "url": "/deploy-react/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "040588820e11c159b5c4",
+    "url": "/deploy-react/static/js/2.567f675f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/deploy-react/static/js/2.567f675f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7994ebc31a9695bec455",
+    "url": "/deploy-react/static/js/main.a3a012ac.chunk.js"
+  },
+  {
+    "revision": "e04f4974b76ccdcb4fab",
+    "url": "/deploy-react/static/js/runtime-main.054f6455.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/deploy-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
